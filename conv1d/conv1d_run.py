130c9ae6943034f4a1452b2f79a13397bbc1e103
Kyle Gerard Felker <kfelker@princeton.edu>
2020-02-18 18:15:08 -0500

    Adapt conv1d/ for running on CUDA devices

--- conv1d/conv1d_run.py
+++ conv1d/conv1d_run.py
@@ -8,27 +8,36 @@ def run(point):
       in_channels = point['in_channels']
       out_channels = point['out_channels']
       kernel_size = point['kernel_size']
-      omp_num_threads = point['omp_num_threads']
+      # omp_num_threads = point['omp_num_threads']
 
-      import os
-      os.environ['OMP_NUM_THREADS'] = str(omp_num_threads)
-      os.environ['MKL_NUM_THREADS'] = str(omp_num_threads)
-      os.environ['KMP_HW_SUBSET'] = '1s,%sc,2t' % str(omp_num_threads)
-      os.environ['KMP_AFFINITY'] = 'granularity=fine,verbose,compact,1,0'
-      os.environ['KMP_BLOCKTIME'] = str(0)
-      #os.environ['MKLDNN_VERBOSE'] = str(1)
+      # import os
+      # os.environ['OMP_NUM_THREADS'] = str(omp_num_threads)
+      # os.environ['MKL_NUM_THREADS'] = str(omp_num_threads)
+      # os.environ['KMP_HW_SUBSET'] = '1s,%sc,2t' % str(omp_num_threads)
+      # os.environ['KMP_AFFINITY'] = 'granularity=fine,verbose,compact,1,0'
+      # os.environ['KMP_BLOCKTIME'] = str(0)
+      ####os.environ['MKLDNN_VERBOSE'] = str(1)
       import torch
 
       print('torch version: ',torch.__version__,' torch file: ',torch.__file__)
 
+      device = torch.device("cuda")
+      torch.backends.cudnn.benchmark = True
 
-      inputs = torch.arange(batch_size * image_size * in_channels,dtype=torch.float).view((batch_size,in_channels,image_size))
-      
-      layer = torch.nn.Conv1d(in_channels,out_channels,kernel_size,stride=1,padding=1)
+      inputs = torch.arange(batch_size * image_size * in_channels,
+                            dtype=torch.float, device=device).view((batch_size,in_channels,image_size))
+
+      layer = torch.nn.Conv1d(in_channels, out_channels, kernel_size,
+                              stride=1,padding=1).to(
+                                 # KGF: unlike linear_run.py, dtype=float causes:
+                                 # RuntimeError: Input type (torch.cuda.FloatTensor) and
+                                 # weight type (torch.cuda.DoubleTensor) should be the same
+                                 # device, float)
+                                 device, dtype=torch.float32)   # = torch.float
       outputs = layer(inputs)
 
       total_flop = kernel_size * in_channels * out_channels * outputs.shape[-1] * batch_size
-      
+
       runs = 5
       tot_time = 0.
       tt = time.time()
@@ -61,8 +70,7 @@ if __name__ == '__main__':
       'in_channels': 3,
       'out_channels': 64,
       'kernel_size': 4,
-      'omp_num_threads':64,
+      # 'omp_num_threads':64,
    }
 
    print('flops for this setting =',run(point))
-
--- conv1d/problem.py
+++ conv1d/problem.py
@@ -6,9 +6,12 @@ Problem.add_dim('image_size',(128,8192))
 Problem.add_dim('in_channels',(2,1024))
 Problem.add_dim('out_channels',(2,1024))
 Problem.add_dim('kernel_size',(2,64))
-Problem.add_dim('omp_num_threads',[64])
+# Problem.add_dim('omp_num_threads',[64])
 
-Problem.add_starting_point(batch_size=10,image_size=128,in_channels=2,out_channels=2,kernel_size=2,omp_num_threads=64)
+Problem.add_starting_point(batch_size=10,image_size=128,
+                           in_channels=2,out_channels=2,kernel_size=2,
+                           )
+                           # omp_num_threads=64)
 
 if __name__ == '__main__':
    print(Problem)
