bf89f19567a0f410264ab8d454d25db554ed6d90
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2021-09-25 13:19:29 +0000

    creating standerdized training method

--- train_models/train.py
+++ train_models/train.py
@@ -13,6 +13,23 @@ def main(args):
         parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate')
         parser.add_argument('--momentum', type=float, default=0.9, help='Momentum')
         parser.add_argument('--epochs', type=int, default=1, help='Epochs to Train on')
-        parser.add_argument('--load_model', type=str, default="model.pt", help='Full path to model *.pt file')
-        parser.add_argument('--save_model', type=str, default="model.pt", help='Full path to desired location for model *.pt file')
+        parser.add_argument('--load_model_cp', type=str, default="model.pt", help='Full path to model *.pt checkpoint file')
+        parser.add_argument('--save_model', type=str, default="model.pt", help='Full path to desired location for model *.pt checkpoint file')
+        parser.add_argument('--model_class_file', type=str, default="model.py", help='Name of file which defines model class (needs to be stored somewhere in this git repo)')
         
+    args = parser.parse_args()
+
+    #load model from class
+    from args.model_class_file import model
+    model = model()
+
+    #load model from checkpoint
+
+    #load dataset
+
+    #train model and check accuracy on validation data after each epoch
+
+    #save model to checkpoint
+
+    
+    # Additional information
