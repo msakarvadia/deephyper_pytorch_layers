87cd39b14ebee2492d031c19e349decc640c9ee8
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2021-10-01 17:04:44 +0000

    fixed directory structure

--- train_models/train.py
+++ train_models/train.py
@@ -48,39 +48,43 @@ def main(args):
         train_data = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, num_workers=2)
 
-<<<<<<< HEAD
         test_data = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=2)
         print("loaded cifar10")
-=======
-        test_data = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
-        testloader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False, num_workers=2)
-        print("loaded cifar100")
->>>>>>> parent of 901503e... added accuracy function
 
     if args.data == "fruits":
         train_data = torchvision.datasets.ImageFolder(root='/home/felker/resnet-ASP/fruits-360/Training', transform=transform)
-        trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, num_workers=2)
+        trainloader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
         test_data = torchvision.datasets.ImageFolder(root='/home/felker/resnet-ASP/fruits-360/Test', transform=transform)
-<<<<<<< HEAD
         testloader = torch.utils.data.DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=2)
-=======
-        testloader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False, num_workers=2)
->>>>>>> parent of 901503e... added accuracy function
         print("loaded fruits")
 
     if args.data == "imagenet":
         train_data = torchvision.datasets.ImageFolder(root='/home/felker/resnet-ASP/imagenet/train', transform=transform)
-        trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True, num_workers=2)
+        trainloader = torch.utils.data.DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
         test_data = torchvision.datasets.ImageFolder(root='/home/felker/resnet-ASP/imagenet/val', transform=transform)
-<<<<<<< HEAD
         testloader = torch.utils.data.DataLoader(test_data, batch_size=args.batch_size, shuffle=False, num_workers=2)
-=======
-        testloader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False, num_workers=2)
->>>>>>> parent of 901503e... added accuracy function
         print("loaded imagenet")
+
+    #define accuracy funcitons
+    def accuracy():
+        #Testing Accuracy
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data[0].to(device), data[1].to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the network on the test images: %d %%' % (
+            100 * correct / total))
+        return 100 * correct / total
+
 
     #train model and check accuracy on validation data after each epoch
     torch.cuda.synchronize()
@@ -159,10 +163,7 @@ if __name__ == "__main__":
     parser.add_argument('--lr', type=float, default=0.001, help='Learning Rate')
     parser.add_argument('--momentum', type=float, default=0.9, help='Momentum')
     parser.add_argument('--epochs', type=int, default=1, help='Epochs to Train on')
-<<<<<<< HEAD
     parser.add_argument('--batch_size', type=int, default=64, help='Epochs to Train on')
-=======
->>>>>>> parent of 901503e... added accuracy function
     parser.add_argument('--load_model_cp', type=str, default="model.pt", help='Full path to model *.pt checkpoint file')
     parser.add_argument('--save_model_cp', type=str, default="model.pt", help='Full path to desired location for model *.pt checkpoint file')
     parser.add_argument('--network_class_file', type=str, default="model.py", help='Name of file which defines model class - class must be named: net() (file needs to be stored somewhere in this git repo)')
